9c4608376318ceda2d14027e68141d1a1e79f60b
Renan Souza <renantrsouza@gmail.com>
2023-01-17 12:07:37 -0400

    first commit: Add configurations to Server

---
+++ server/src/server.ts
@@ -0,0 +1,24 @@
+import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+import cors from "@fastify/cors";
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.register(cors);
+
+app.get("/", async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: 'Beber'
+      }
+    }
+  });
+  return habits;
+});
+
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP Server running!");
+});
