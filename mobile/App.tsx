dbdf4d9fcb98f79a8fe824de9ea61e624252f479
Renan Souza <renantrsouza@gmail.com>
2023-01-20 12:48:58 -0400

    refact: iport components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
-//import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import "./src/lib/dayjs";
+import { StatusBar } from 'react-native';
 import { 
   useFonts, 
   Inter_400Regular, 
@@ -8,6 +8,7 @@ import {
   Inter_800ExtraBold  
 } from "@expo-google-fonts/inter";
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,22 +23,10 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Renan Souza</Text>
+    <>
+      <Home />
       <StatusBar barStyle={'light-content'} backgroundColor="transparent" translucent/>
-    </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
