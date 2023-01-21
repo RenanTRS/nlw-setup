65f73a0f5343c33acd7071d6ec7f92fb721ac21f
Renan Souza <renantrsouza@gmail.com>
2023-01-21 11:06:15 -0400

    add: Use radix popover

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -2,6 +2,17 @@ import * as Popover from "@radix-ui/react-popover";
 
 export function HabitDay() {
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    <Popover.Root>
+      <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+
+      <Popover.Portal>
+        <Popover.Content className="text-white min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+          <span className="font-semibold text-zinc-400">terça-feira</span>
+          <span className="mt-1 font-extrabold leading-tight text-3xl">21/01</span>
+
+          <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   );
 }
