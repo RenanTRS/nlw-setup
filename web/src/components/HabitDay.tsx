9233721f9b9289d3f78b562bee06dad513c8c105
Renan Souza <renantrsouza@gmail.com>
2023-01-21 10:43:57 -0400

    import: popover in the habitday component

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,3 +1,5 @@
+import * as Popover from "@radix-ui/react-popover";
+
 export function HabitDay() {
   return (
     <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
