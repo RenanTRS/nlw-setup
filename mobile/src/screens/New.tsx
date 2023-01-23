e9fc7f87c89b319bfe562411a76fc70b1130b7da
Renan Souza <renantrsouza@gmail.com>
2023-01-23 13:40:36 -0400

    add: Button to confim

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
-import { ScrollView, Text, TextInput, View } from "react-native";
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import colors from "tailwindcss/colors";
 import { BackButton } from "../components/BackButton";
 import { CheckBox } from "../components/CheckBox";
+import {Feather} from "@expo/vector-icons"
 
 export function New() {
   const [weekDays, setWeekDays] = useState<number[]>([]);
@@ -51,6 +53,21 @@ export function New() {
           />
 
         ))}
+
+        <TouchableOpacity
+        activeOpacity={0.7}
+        className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+        >
+          <Feather 
+            name="check"
+            size={20}
+            color={colors.white}
+          />
+
+          <Text className="font-semibold text-base text-white ml-2">
+            Confirmar
+          </Text>
+        </TouchableOpacity>
       </ScrollView>
 
     </View>
