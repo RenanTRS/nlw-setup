24e7aa412ca7ef8de8c40e7f277df8ff1d25385d
Renan Souza <renantrsouza@gmail.com>
2023-01-23 12:25:29 -0400

    add: Navigate function in the touchable

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -2,8 +2,11 @@ import { View, TouchableOpacity, Text } from "react-native";
 import Logo from "../assets/logo.svg";
 import {Feather} from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
+import {useNavigation} from "@react-navigation/native";
 
 export function Header() {
+  const {navigate} = useNavigation();
+
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
@@ -11,6 +14,7 @@ export function Header() {
       <TouchableOpacity
         activeOpacity={0.7}
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
+        onPress={() => navigate('new')}
       >
         <Feather 
           name="plus"
