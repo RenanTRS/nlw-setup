0b42f2ad5e0aafa6b4b81fb813729a778c8f350f
Renan Souza <renantrsouza@gmail.com>
2023-01-23 12:27:42 -0400

    add: ScrollView  in the New route

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,8 +1,11 @@
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 
 export function New() {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView showsVerticalScrollIndicator={false}>
+
+      </ScrollView>
 
     </View>
   );
