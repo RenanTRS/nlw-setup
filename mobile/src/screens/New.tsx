a54df040821be2312bdef097629c80d92b05dda1
Renan Souza <renantrsouza@gmail.com>
2023-01-23 13:38:58 -0400

    add: ContentContainerStyle padding bottom

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -18,7 +18,10 @@ export function New() {
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView 
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: 100}}
+      >
         <BackButton />
 
         <Text className="mt-6 text-white font-extrabold text-3xl">
