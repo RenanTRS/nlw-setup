648d4870c6031b070f9c6cabcdf1009a8925c40b
Renan Souza <renantrsouza@gmail.com>
2023-01-23 12:41:12 -0400

    add: import backbutton and add texts and inputs

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,9 +1,23 @@
-import { ScrollView, View } from "react-native";
+import { ScrollView, Text, TextInput, View } from "react-native";
+import { BackButton } from "../components/BackButton";
 
 export function New() {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <ScrollView showsVerticalScrollIndicator={false}>
+        <BackButton />
+
+        <Text className="mt-6 text-white font-extrabold text-3xl">
+          Criar hábito
+        </Text>
+
+        <Text className="mt-6 text-white font-semibold text-base">
+          Qual seu comprometimento?
+        </Text>
+
+        <TextInput 
+          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600"
+        />
 
       </ScrollView>
 
