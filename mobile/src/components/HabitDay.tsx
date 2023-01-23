032d6311cdd5ad60c55aa5067d73bce5bf1157ed
Renan Souza <renantrsouza@gmail.com>
2023-01-23 14:24:55 -0400

    add: import touchableopacityprops

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -1,4 +1,4 @@
-import { TouchableOpacity, Dimensions } from "react-native";
+import { TouchableOpacity, Dimensions, TouchableOpacityProps } from "react-native";
 
 const weekDays = 7;
 const screenHorizontalPadding = (32 * 2) / 5;
@@ -6,12 +6,15 @@ const screenHorizontalPadding = (32 * 2) / 5;
 export const dayMarginBetween = 8;
 export const daySize = (Dimensions.get('screen').width / weekDays) - (screenHorizontalPadding + 5); //pega um tamanho ideal de acordo com o tamanho da tela do dispositivo
 
-export function HabitDay() {
+interface Props extends TouchableOpacityProps {}
+
+export function HabitDay({...rest}: Props) {
   return (
     <TouchableOpacity 
       className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
       style={{width: daySize, height: daySize}}
       activeOpacity={0.7}
+      {...rest}
     />
   );
 }
