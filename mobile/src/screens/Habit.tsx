afdd175dc8322524ff82a099b47ffd1b8e8cd840
Renan Souza <renantrsouza@gmail.com>
2023-01-23 14:43:54 -0400

    add: backbutton, text, progressbar and checkbox

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -19,6 +19,34 @@ export function Habit() {
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: 100}}
+      >
+        <BackButton />
+
+        <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
+          {dayOfWeek}
+        </Text>
+
+        <Text className="text-white font-extrabold text-3xl">
+          {dayAndMonth}
+        </Text>
+
+        <ProgressBar progress={55}/>
+
+        <View className="mt-6">
+          <CheckBox
+            title="Beber 2L de água"
+            checked={false}
+          />
+          <CheckBox 
+            title="Caminhar"
+            checked={true}
+          />
+        </View>
+      </ScrollView>
+
 
     </View>
   );
