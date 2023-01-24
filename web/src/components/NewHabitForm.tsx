12b6729280409b43205524036873caa6b46be271
Renan Souza <renantrsouza@gmail.com>
2023-01-24 11:47:37 -0400

    add: Import and use checkbox component

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,6 +1,9 @@
 import { Check } from "phosphor-react";
+import { Checkbox } from "./Checkbox";
 
 export function NewHabitForm () {
+  const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+
   return (
     <form className="w-full flex flex-col mt-6">
       <label htmlFor="title" className="font-semibold leading-tight">
@@ -18,6 +21,13 @@ export function NewHabitForm () {
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Qual a recorrência?
       </label>
+
+      <div className="flex flex-col gap-2 mt-3">
+        {availableWeekDays.map(weekDay => (
+          <Checkbox key={weekDay} title={weekDay} form />
+        ))
+        }
+      </div>
 
       <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
         <Check size={20} weight="bold" />
