f1a37b485aa7d04bb71ebe0fb8b6ffd42fa51e0d
Renan Souza <renantrsouza@gmail.com>
2023-01-25 12:12:42 -0400

    add: Transform language of dayjs in portuguese

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import {App} from './App';
 
 import "./styles/global.css";
+import "./lib/dayjs";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
