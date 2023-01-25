76281232777c7fabdec2ca139047693c8d1293c2
Renan Souza <renantrsouza@gmail.com>
2023-01-25 11:34:50 -0400

    add: itens in the interface

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -4,12 +4,13 @@ import clsx from "clsx";
 import { Checkbox } from "./Checkbox";
 
 interface HabitDayProps {
-  completed: number;
-  amount: number;
+  date: Date;
+  completed?: number;
+  amount?: number;
 }
 
-export function HabitDay({completed, amount}: HabitDayProps) {
-  const completedPercentage = Math.round((completed / amount) * 100);
+export function HabitDay({completed = 0, amount = 0}: HabitDayProps) {
+  const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0;
   return (
     <Popover.Root>
       <Popover.Trigger 
