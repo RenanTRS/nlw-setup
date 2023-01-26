66cfb0d893b4e24ccc2967e5c9f940d156e83e83
Renan Souza <renantrsouza@gmail.com>
2023-01-26 10:15:21 -0400

    refact: add new params to HabitDay component

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -6,9 +6,13 @@ const screenHorizontalPadding = (32 * 2) / 5;
 export const dayMarginBetween = 8;
 export const daySize = (Dimensions.get('screen').width / weekDays) - (screenHorizontalPadding + 5); //pega um tamanho ideal de acordo com o tamanho da tela do dispositivo
 
-interface Props extends TouchableOpacityProps {}
+interface Props extends TouchableOpacityProps {
+  date: Date;
+  amount?: number;
+  completed?: number;
+}
 
-export function HabitDay({...rest}: Props) {
+export function HabitDay({amount = 0, completed = 0, date,...rest}: Props) {
   return (
     <TouchableOpacity 
       className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
