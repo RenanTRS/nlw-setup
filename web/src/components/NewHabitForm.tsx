476bb957a344448b05fe9a573de499727a6c2acb
Renan Souza <renantrsouza@gmail.com>
2023-01-27 15:00:07 -0400

    style: Add transion colors in the button from form

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -71,7 +71,7 @@ export function NewHabitForm () {
         }
       </div>
 
-      <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
+      <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors">
         <Check size={20} weight="bold" />
         Confirmar
       </button>
