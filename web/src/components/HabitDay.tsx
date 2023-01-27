a90f820e12c88c053595395db5b90ee5d64b0c98
Renan Souza <renantrsouza@gmail.com>
2023-01-27 11:04:32 -0400

    add:style: A white border when in the current day

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -25,7 +25,8 @@ export function HabitDay({date, completed = 0, amount = 0}: HabitDayProps) {
           "bg-violet-800 border-violet-600": completedPercentage >= 20 && completedPercentage < 40,
           "bg-violet-700 border-violet-500": completedPercentage >= 40 && completedPercentage < 60,
           "bg-violet-600 border-violet-500": completedPercentage >= 60 && completedPercentage < 80,
-          "bg-violet-500 border-violet-400": completedPercentage >= 80
+          "bg-violet-500 border-violet-400": completedPercentage >= 80,
+          "border-white border-3": currentDay
         })}
       />
 
